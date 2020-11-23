188dfb51579641092ebb6e20b1ddcbb70b3c7716
tanya <tanya.orlova17@gmail.com>
2020-11-23 17:16:48 +0300

    Скрытие статус бара

--- app/src/main/java/com/magicunderhood/androidacademyhomework/MovieDetailsActivity.kt
+++ app/src/main/java/com/magicunderhood/androidacademyhomework/MovieDetailsActivity.kt
@@ -1,11 +1,18 @@
 package com.magicunderhood.androidacademyhomework
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 }
