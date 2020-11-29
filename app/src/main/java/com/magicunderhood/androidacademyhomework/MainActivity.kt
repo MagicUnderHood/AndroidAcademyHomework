7c01c0886b22183185b972abfc08268fd00a463f
tanya <tanya.orlova17@gmail.com>
2020-11-29 22:21:46 +0300

    Исправлено направление градиента, скрыт статус бар

--- app/src/main/java/com/magicunderhood/androidacademyhomework/MainActivity.kt
+++ app/src/main/java/com/magicunderhood/androidacademyhomework/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.magicunderhood.androidacademyhomework
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(),
@@ -10,6 +12,12 @@ class MainActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
 
         supportFragmentManager
                 .beginTransaction()
